32d4719488cedf9b810123dc20eb98786dfc4bd6
Marco Giammari <marcogiammari@gmail.com>
2023-05-30 15:42:55 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+  <h1 class="text-3xl font-bold underline">Prova</h1>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+<template>
+  <header>
+
+  </header>
+</template>
+
+<style lang="scss" scoped></style>
